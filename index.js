f4936f4dac946425420fa4dea3332c0978cfce08
betty-bucket <coulson.ev@gmail.com>
2020-09-08 18:53:54 +1200

    add rows const that brings back basic demographic info

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 getData()
 async function getData () {
-  const response = await fetch('testData1.csv')
+  const response = await fetch('censusDataClean.csv')
   const data = await response.text()
   console.log(data)
 
   const rows = data.split('\n').slice(1)
-  rows.forEach(row => {
-    console.log('row: ', row)
+  rows.forEach(elt => {
+    const row = elt.split(',')
+    const id = row[0]
+    const gender = row[3]
+    const region = row[4]
+    const ageGroup = row[5]
+    console.log('id: ', id, 'gender: ', gender, 'region: ', region, 'ageGroup: ', ageGroup)
   })
-  console.log('rows: ', rows)
 }
