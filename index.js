dc53f45d75c3cacc6258eba92725a009017e32e5
betty-bucket <coulson.ev@gmail.com>
2020-09-13 13:12:46 +1200

    add information to index files

--- index.js
+++ index.js
@@ -1,51 +1,27 @@
-var noderequire = require('node-require')
+const xlables = []
+const ylables = []
 
-var Chart = require('dist/chart.js')
+chartIt()
 
-getData()
-async function getData () {
-  const response = await fetch('censusDataClean.csv')
-  const data = await response.text()
-  console.log(data)
-
-  const table = data.split('\n').slice(1)
-  table.forEach(row => {
-    const cols = row.split(',')
-    const id = cols[0]
-    const gender = cols[3]
-    const region = cols[4]
-    const ageGroup = cols[5]
-    console.log('id: ', id, 'gender: ', gender, 'region: ', region, 'ageGroup: ', ageGroup)
-  })
-}
-
-define(function (require, exports, module) {
-  var ctx = document.getElementById('myChart')
-  var myChart = new Chart(ctx, {
-    type: 'bar',
+async function chartIt () {
+  await getData()
+  const ctx = document.getElementById('myChart')
+  const myChart = new Chart(ctx, {
+    type: 'pie',
     data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      labels: ['Female', 'Male', 'Gender non Binary', 'Prefer to self identify' ],
       datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
+        data: [10, 5, 2, 1],
+        label: ['f'],
         backgroundColor: [
           'rgba(255, 99, 132, 0.2)',
           'rgba(54, 162, 235, 0.2)',
           'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
-        ],
-        borderWidth: 1
+          'rgba(75, 192, 192, 0.2)'
+        ]
       }]
+
+    // These labels appear in the legend and in the tooltips when hovering different arcs
     },
     options: {
       scales: {
@@ -57,7 +33,22 @@ define(function (require, exports, module) {
       }
     }
   })
+}
 
-  myChart()
-  // Put traditional CommonJS module content here
-})
+async function getData () {
+  const response = await fetch('censusDataClean.csv')
+  const data = await response.text()
+  console.log(data)
+
+  const table = data.split('\n').slice(1)
+  table.forEach(row => {
+    const cols = row.split(',')
+    const id = cols[0]
+    const gender = cols[3]
+    xlables.push(gender)
+    const region = cols[4]
+    ylables.push(region)
+    const ageGroup = cols[5]
+    console.log('id: ', id, 'gender: ', gender, 'region: ', region, 'ageGroup: ', ageGroup)
+  })
+}
